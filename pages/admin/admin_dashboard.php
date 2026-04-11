e380873e871776e211e3d4b325a3e7e101bf5263
Tonny <casidsid.marietonny888@gmail.com>
2026-04-11 17:43:44 +0800

    tonny041126-1743

--- pages/admin/admin_dashboard.php
+++ pages/admin/admin_dashboard.php
@@ -128,7 +128,7 @@ $activeQueue = $dashboardStats["activeQueue"];
       <article class="card">
         <div class="icon icon--customer-list" aria-hidden="true">
           <img
-            src="<?= project_url('/pages/admin/IMAGES/LANDING_CUSTOMER_LIST.png') ?>"
+            src="<?= project_url('/pages/admin/IMAGES/LANDING_CUSTOMER_LIST.png?v=20260411h4') ?>"
             alt=""
             class="icon-image icon-image--customer-list"
           >
