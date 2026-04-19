9f8654b5552b0c174826ca91415c039a137b0dc2
la-luma <galdo.luisamarie1@gmail.com>
2026-04-19 11:44:43 +0800

    deleted the print order in customer dash

--- main/customer_dash.php
+++ main/customer_dash.php
@@ -730,16 +730,6 @@ $dashboardQueues = [
       </div>
     </a>
 
-    <a href="/main/custo_print_order.php" class="quick-card-link">
-      <div class="quick-card">
-        <div class="quick-icon-box">
-          <img src="/assets/images/LANDING_PRINT-ORD.png" alt="Print Order" class="quick-icon">
-        </div>
-        <h4>Print Order</h4>
-        <p>Place an order to print your document.</p>
-      </div>
-    </a>
-
     <a href="/main/custo_edit_profile.php" class="quick-card-link">
       <div class="quick-card">
         <div class="quick-icon-box">
