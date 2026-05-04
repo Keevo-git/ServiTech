01022087c3c3051d6094ad2fd700f560abd2e8a1
Tonny <casidsid.marietonny888@gmail.com>
2026-05-05 00:40:46 +0800

    tonny050526-0040

--- pages/admin/customer_list/custoL.php
+++ pages/admin/customer_list/custoL.php
@@ -37,7 +37,7 @@ function customer_code_from_id(int $id): string {
   <link rel="icon" type="images/png" href="/assets/images/favicon.png" >
   <link rel="stylesheet" href="<?= admin_url('/assets/css/style.css?v=20260315h2') ?>">
   <link rel="stylesheet" href="<?= admin_url('/pages/admin/admin.css?v=20260315h2') ?>">
-  <link rel="stylesheet" href="<?= admin_url('/pages/admin/customer_list/custoL.css?v=20260411h2') ?>">
+  <link rel="stylesheet" href="<?= admin_url('/pages/admin/customer_list/custoL.css?v=20260411h3') ?>">
 </head>
 
 <body>
@@ -127,11 +127,11 @@ function customer_code_from_id(int $id): string {
   </main>
 
   <div class="cl-modalOverlay" id="msgModal">
-    <div class="cl-modalCard" role="dialog" aria-modal="true">
+    <div class="cl-modalCard" role="dialog" aria-modal="true" aria-labelledby="messageModalTitle">
       <button class="cl-modalX" type="button" id="closeModal" aria-label="Close modal">&times;</button>
 
       <div class="cl-modalHead">
-        <h3>Send Message to Customer</h3>
+        <h3 id="messageModalTitle">Send Message to Customer</h3>
       </div>
 
       <div class="cl-infoCard">
