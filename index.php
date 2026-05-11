a34ccae9c67f4f959676d1c14eb832376009e56e
La-luma <galdo.luisamarie1@gmail.com>
2026-05-11 15:54:05 +0800

    dada

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ try {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>ServiTech: JC Repair Shop</title>
   <link rel="icon" type="images/png" href="<?= htmlspecialchars(servitech_url('/assets/images/favicon.png'), ENT_QUOTES, 'UTF-8') ?>" >
-  <link rel="stylesheet" href="<?= htmlspecialchars(servitech_url('/assets/css/style.css?v=20260511announce2'), ENT_QUOTES, 'UTF-8') ?>">
+  <link rel="stylesheet" href="<?= htmlspecialchars(servitech_url('/assets/css/style.css?v=20260511announce3'), ENT_QUOTES, 'UTF-8') ?>">
 </head>
 <body>
 
