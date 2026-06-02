b6fe4b4c0d0b359f81c6d4787f98aa92acc47ab8
la-luma <galdo.luisamarie1@gmail.com>
2026-06-02 13:06:24 +0800

    fixed login pt2

--- auth/_shared.php
+++ auth/_shared.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../config/app.php";
 require_once __DIR__ . "/../config/mail.php";
 
-const AUTH_UI_VERSION = "20260602auth6";
+const AUTH_UI_VERSION = "20260602auth7";
 
 if (!function_exists("auth_url_raw")) {
     function auth_url_raw(string $path = "/"): string
