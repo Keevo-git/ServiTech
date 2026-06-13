822bb23359a7bfb5ebc66fb2ed500da121eb07d2
Tonny <casidsid.marietonny888@gmail.com>
2026-06-14 03:31:48 +0800

    tonny061426-0331

--- pages/admin/Services/edit_services.php
+++ pages/admin/Services/edit_services.php
@@ -99,7 +99,7 @@ function h($s){ return htmlspecialchars((string)$s, ENT_QUOTES, "UTF-8"); }
   <link rel="icon" type="images/png" href="/assets/images/favicon.png" >
   <link rel="stylesheet" href="<?= admin_url('/assets/css/style.css?v=20260315h2') ?>">
   <link rel="stylesheet" href="<?= admin_url('/pages/admin/admin.css?v=20260612header-global-type') ?>">
-  <link rel="stylesheet" href="<?= admin_url('/pages/admin/Services/manage_services.css?v=20260614-uniform-tabs') ?>">
+  <link rel="stylesheet" href="<?= admin_url('/pages/admin/Services/manage_services.css?v=20260614-matching-tabs') ?>">
 </head>
 <body>
 
