1dfbadcfa1e4bc3b997be9834d51ae0208793ba4
Tonny <casidsid.marietonny888@gmail.com>
2026-06-18 20:19:57 +0800

    tonny061826- remember me alignment

--- auth/log_in.php
+++ auth/log_in.php
@@ -11,7 +11,7 @@ $csrfToken = servitech_csrf_token();
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>ServiTech: Login</title>
   <?= servitech_favicon_link() ?>
-  <link rel="stylesheet" href="<?= auth_url("/assets/css/style.css?v=20260618-remember-row") ?>">
+  <link rel="stylesheet" href="<?= auth_url("/assets/css/style.css?v=20260618-remember-row-lock") ?>">
 </head>
 <body class="auth-page auth-page--login">
 
