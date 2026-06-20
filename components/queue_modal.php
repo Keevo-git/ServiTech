580fc5faedc8904ef1b4193605dfd823b1c06e52
Tonny <casidsid.marietonny888@gmail.com>
2026-06-20 21:26:58 +0800

    tonny062026- modal close button

--- components/queue_modal.php
+++ components/queue_modal.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/assets/css/queue-modal.css?v=20260602-stable-overlay">
+<link rel="stylesheet" href="/assets/css/queue-modal.css?v=20260620-centered-close">
 <div id="queueModal" class="modal-overlay queue-modal-overlay" style="display:none;" aria-hidden="true">
   <div class="modal queue-success-modal" role="dialog" aria-modal="true" aria-labelledby="queueModalTitle" aria-describedby="queueModalMessage queueModalNote" tabindex="-1">
     <button id="queueModalCloseBtn" type="button" class="queue-success-modal__close" aria-label="Close joined queue confirmation">&times;</button>
