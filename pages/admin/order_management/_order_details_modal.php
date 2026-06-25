34635cce3cff5390cecafdb42474c7701e219347
la-luma <galdo.luisamarie1@gmail.com>
2026-06-25 08:13:11 +0800

    remove send back pt3

--- pages/admin/order_management/_order_details_modal.php
+++ pages/admin/order_management/_order_details_modal.php
@@ -69,9 +69,9 @@
 
         <div class="order-modal-action-stack">
           <div class="order-modal-actions">
-            <button class="om-btn om-btn--primary order-modal-update" type="button" id="omCancel">Cancel</button>
+            
           </div>
-
+          <button class="om-btn om-btn--light" type="button" id="omCancel">Cancel</button>
           <button class="om-btn om-btn--primary order-modal-update" type="button" id="omSave">Update</button>
 
           <button
